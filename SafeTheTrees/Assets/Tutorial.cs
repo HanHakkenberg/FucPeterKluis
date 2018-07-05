5efb4d275c7b45ec0e838415bd98cc22f0bf215d
HanHakkenbergDeltion <han.hakkenberg@gmail.com>
2018-07-05 17:21:35 +0200

    UI Animations

---
+++ SafeTheTrees/Assets/Tutorial.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+	public Animator myAnimator;
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			myAnimator.SetInteger("l", myAnimator.GetInteger("l")+ 1);
+			if (myAnimator.GetInteger("l") >= 3) {
+				Destroy(this);
+			}
+		}
+	}
+}
